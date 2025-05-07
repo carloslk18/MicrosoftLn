1788fb3543014db2ee36abc74e32a698d1809f23
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-05-06 23:48:32 -0300

    Update Examples.cs

--- FourthUnit/Examples.cs
+++ FourthUnit/Examples.cs
@@ -17,6 +17,48 @@ public class Examples{
         char[] valueArray = value.ToCharArray();
         Array.Reverse(valueArray);
 
+
+                    string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);
+
+        string result = String.Join(",",valueArray);
+        Console.WriteLine(result);
+
+        string[] items = result.Split(',');
+        foreach (string item in items){
+            Console.WriteLine(item);
+
+             string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);        string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);
+
+        string result = String.Join(",",valueArray);
+        Console.WriteLine(result);
+
+        string[] items = result.Split(',');
+        foreach (string item in items){
+            Console.WriteLine(item);
+
+             string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);        string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);
+
+        string result = String.Join(",",valueArray);
+        Console.WriteLine(result);
+
+        string[] items = result.Split(',');
+        foreach (string item in items){
+            Console.WriteLine(item);
+
+             string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);
+
         }
     }
 }
