124f63fd7457026f17d56803592c80fe9a2a0fbf
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-05-08 21:34:54 -0300

    Update Examples.cs

--- FourthUnit/Examples.cs
+++ FourthUnit/Examples.cs
@@ -51,13 +51,6 @@ public class Examples{
         string result = String.Join(",",valueArray);
         Console.WriteLine(result);
 
-        string[] items = result.Split(',');
-        foreach (string item in items){
-            Console.WriteLine(item);
-
-             string value = "abc123";
-        char[] valueArray = value.ToCharArray();
-        Array.Reverse(valueArray);
 
         }
     }
